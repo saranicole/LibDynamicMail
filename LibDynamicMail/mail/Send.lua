e6ef1009c9974a90adfe29cd0192f553d430e594
Sara Jarjoura <saranicole1980@gmail.com>
2026-02-04 22:09:20 -0500

    fix: Try using call later

--- LibDynamicMail/mail/Send.lua
+++ LibDynamicMail/mail/Send.lua
@@ -14,6 +14,13 @@ local function NormalizeAccountName(name)
     return name
 end
 
+local function populateGamepadSend(parsedRecipient, parsedSubject, parsedBody)
+  mailSend.initialBodyInsertText = parsedBody
+  mailSend.initialContact = parsedRecipient
+  mailSend.initialSubject = parsedSubject
+  ZO_GamepadGenericHeader_SetActiveTabIndex(MAIN_MENU_GAMEPAD.header, 2)
+end
+
 function LDM:PopulateCompose(templateName, values)
   local template = self:GetTemplate(templateName)
   if not template then d("|cFF0000[LDM]|r LibDynamicMail: Template not found") return false end
@@ -38,11 +45,10 @@ function LDM:PopulateCompose(templateName, values)
   local decoratedRecipient = NormalizeAccountName(parsedRecipient)
 
   if IsInGamepadPreferredMode() or IsConsoleUI() then
-    mailSend.initialBodyInsertText = parsedBody
-    mailSend.initialContact = parsedRecipient
-    mailSend.initialSubject = parsedSubject
     MAIN_MENU_GAMEPAD:ShowScene("mailGamepad")
-    ZO_GamepadGenericHeader_SetActiveTabIndex(MAIN_MENU_GAMEPAD.header, 2)
+    zo_callLater(function()
+      populateGamepadSend(parsedRecipient, parsedSubject, parsedBody)
+    end, 500)
   else
     mailSend.to:SetText(decoratedRecipient)
     mailSend.subject:SetText(parsedSubject)
